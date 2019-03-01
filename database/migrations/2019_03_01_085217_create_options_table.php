3582f9fa311bc6aff143a9be2c95302426e4f504
TomasSamu <tomas.samu@gmail.com>
2019-03-01 10:05:07 +0100

    Option model created

---
+++ database/migrations/2019_03_01_085217_create_options_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('question_id');
+            $table->unsignedInteger('user_id');
+            $table->text('title');
+            $table->string('description');
+            $table->timestamps();
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}
