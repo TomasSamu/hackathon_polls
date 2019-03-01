7ba4697295f2109f46db7a88c772ae120f33c805
Ale <ale.spreafico@yandex.com>
2019-03-01 11:20:26 +0000

    best way to put user validations

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -9,15 +9,6 @@
         @foreach ($questions as $question)
             <li>{{ $question->title }}</li>
             <br>
-            @guest
-                <h2>You need to Sign In to Vote!</h2>
-            @else
-                <ul>
-                    <li><a href="{{ action('PublicController@show', $question->id) }}">Read Question</a></li>
-                    <li><a href="#">Vote</a></li>
-                    <li><a href="#">Add Option</a></li>
-                </ul>
-            @endguest
         @endforeach
     </ul>
 @endsection
--- resources/views/public/show.blade.php
+++ resources/views/public/show.blade.php
@@ -3,4 +3,13 @@
 @section('content')
     <h2>{{ $question->title }}</h2>
     <p>{{ $question->text }}</p>
+    @guest
+        <h2>You need to Sign In to Vote!</h2>
+    @else
+        <ul>
+            <li><a href="{{ action('PublicController@show', $question->id) }}">Read Question</a></li>
+            <li><a href="#">Vote</a></li>
+            <li><a href="#">Add Option</a></li>
+        </ul>
+    @endguest
 @endsection
